db92e51f4d3297bb2da875a0f5d429a5a2848ab0
Nikki Little <nikkilittle13@gmail.com>
2024-04-15 13:47:25 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_sequence = []
+
+    if length == 0:
+        print([])
+        return
+
+    if length == 1:
+        print([0])
+        return
+
+    fib_sequence.append(0)
+    fib_sequence.append(1)
+
+    for _ in range(length - 2):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    print(fib_sequence)
     pass
